cec3e1108151f9c475d4ffad4366f93e16edbfef
Tom OB <tobrien999@gmail.com>
2013-02-12 16:37:27 -0800

    adding fileshit'

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,8 @@ var app = {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.report('deviceready');
+		alert("DeviceReady");
+		getFileSystem();
     },
     report: function(id) {
         // Report the event in the console
